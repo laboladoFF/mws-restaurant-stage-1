b027d9dc58cb0304d5317d1a0dae0f915079b7dd
laboladoFF <labolado@163.com>
2018-11-18 23:40:21 +0800

    install ServiceWorker

--- js/main.js
+++ js/main.js
@@ -4,6 +4,19 @@ let restaurants,
 var newMap
 var markers = []
 
+/*============== 注册ServiceWorker ================*/
+
+if('serviceWorker' in navigator){
+  navigator.serviceWorker.register('/sw.js').then(function(registration){
+      console.log('ServiceWorker registration successful with scope:', registration.scope);
+      if(registration.waiting){
+
+      }
+  }).catch(function(err){
+      console.log('ServiceWorker registration failed:', err);
+  });
+}
+
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
